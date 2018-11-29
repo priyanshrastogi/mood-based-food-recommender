dca13ee4e62fc54e0b1dd43ecc9a82fda0763fca
Priyansh Rastogi <priyanshrastogi54@gmail.com>
2018-11-30 00:54:07 +0530

    showing recommendations done

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ stop.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}
 from nltk.stem import WordNetLemmatizer
 
 food_data = pd.read_csv('food_choices.csv')
+res_data = pd.read_csv('zomato.csv', encoding='latin-1')
+res_data = res_data.loc[(res_data['Country Code'] == 1) & (res_data['City'] == 'New Delhi'), :]
+res_data = res_data.loc[res_data['Longitude'] != 0, :]
+res_data = res_data.loc[res_data['Latitude'] != 0, :]
+res_data = res_data.loc[res_data['Latitude'] < 29] # clearing out invalid outlier
+res_data = res_data.loc[res_data['Rating text'] != 'Not rated']
+res_data['Cuisines'] = res_data['Cuisines'].astype(str)
 
 def search_comfort(mood):
   lemmatizer = WordNetLemmatizer()
@@ -24,7 +31,6 @@ def search_comfort(mood):
   sorted_food = sorted(foodcount, key=foodcount.get, reverse=True)
   return sorted_food
 
-
 def find_my_comfort_food(mood):
   topn = []
   topn = search_comfort(mood) #function create dictionary only for particular mood
@@ -41,4 +47,28 @@ def find_restaurant():
   mood = request.args.get('mood')
   result = find_my_comfort_food(mood)
   result_str = 'You should eat {}, {}, or {}.'.format(result[0], result[1], result[2])
-  return render_template('result.html', result = result_str, mood = mood)
+  food_to_cuisine_map = {
+    "pizza": "pizza",
+    "ice cream": "ice cream",
+    "chicken wings": "mughlai",
+    "chinese": "chinese",
+    "chip": "bakery",
+    "chocolate": "bakery",
+    "candy": "bakery",
+    "mcdonalds": "burger",
+    "burger": "burger",
+    "cooky": "bakery",
+    "mac and cheese": "american",
+    "pasta": "italian",
+    "soup": "chinese",
+    "dark chocolate": "bakery",
+    "terra chips" : "bakery",
+    "reese's cups(dark chocolate)": "bakery"
+  }
+  restaurants_list = []
+  for item in result:
+    restaurants = res_data[res_data.Cuisines.str.contains(food_to_cuisine_map[item], case=False)].sort_values(by='Aggregate rating', ascending=False).head(3)
+    restaurants_list.append(restaurants.iloc[0])
+    restaurants_list.append(restaurants.iloc[1])
+    restaurants_list.append(restaurants.iloc[2])
+  return render_template('result.html', result = result_str, mood = mood, restaurants1 = restaurants_list[:3], restaurants2 = restaurants_list[3:6], restaurants3 = restaurants_list[6:])
